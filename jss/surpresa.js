13941254c53d91fe721929754fdd693393e7fa6f
mariazorteaz <128661477+mariazorteaz@users.noreply.github.com>
2023-06-12 20:32:03 -0300

    Add files via upload

---
+++ jss/surpresa.js
@@ -0,0 +1,13 @@
+let amor = prompt("Quanto você me ama hoje?");
+
+if (amor < 80) {
+ alert("Não gostei, tá chatinha em, Eu amo mais:[");
+} else if (amor >= 80 && amor <= 95) {
+  alert("Gostei, mas ainda pode ser melhor. Eu amo mais:)");
+} else if (amor > 95) {
+  alert("Ótimo! Mas eu ainda amo mais meu beem:3");
+}
+
+
+
+
